6084dc0e7eed28d46fc9ecb44e2739084eca60d7
Divyansh74 <84952780+Divyansh74@users.noreply.github.com>
2022-08-07 20:48:27 +0530

    Update raspi.py

--- raspi.py
+++ raspi.py
@@ -1,12 +1,21 @@
-from ipaddress import ip_address
-import psutil
- 
- 
-ip_address = psutil.net_if_addrs()
-print('The IP Address is:', ip_address['wlp2s0'][0][1])
+import subprocess
+s,r = subprocess.getstatusoutput(f'cat /sys/class/thermal/thermal_zone*/type')
+x=r.split("\n")
+d,t=subprocess.getstatusoutput(f'cat /sys/class/thermal/thermal_zone*/temp')
+y=t.split("\n")
+dic={}
+for i in range(0,len(x)):
+    dic[x[i]] = y[i]
 
-print('The CPU usage is: ', psutil.cpu_percent(4))
-print('RAM memory % used:', psutil.virtual_memory()[2])
-print('The Cpu Frequency is: ',psutil.cpu_freq()[0])
-temp=psutil.sensors_temperatures()
-print('The CPU Temperature is:',temp['pch_cannonlake'][0][1])
+print(f'The Cpu temperature is: {int(dic["x86_pkg_temp"])/1000}' + "°C")
+
+
+# print("{}".format(str(t)))
+a,b=subprocess.getstatusoutput("hostname -I | awk '{print $1}'")
+f,g=subprocess.getstatusoutput("cat /proc/cpuinfo | grep MHz")
+z,y=subprocess.getstatusoutput("free")
+print("Memory Usage",y)
+print("The IP Address is:", b)
+print("Cpu Frequency\n", g)
+# print(subprocess.getstatusoutput(f'cat /sys/class/thermal/thermal_zone*/type'))
+# print(r.length())
